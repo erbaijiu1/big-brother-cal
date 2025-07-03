cf99d9b336eb93c8f86f18b93b3cd0d523845843
yyyhcyang <yyyhcyang@jintengtech.com>
2025-07-03 17:37:44 +0800

    路径支持配置

--- uniapp-cal-price/src/common/config.ts
+++ uniapp-cal-price/src/common/config.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:8088'; // 替换为你的实际后端域名
+// export const BASE_URL = 'http://localhost:8088'; // 替换为你的实际后端域名
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
