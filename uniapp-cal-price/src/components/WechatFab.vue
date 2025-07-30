0e0d169244bbe4e79720a91a519b00ac59a55d72
yyyhcyang <yyyhcyang@jintengtech.com>
2025-07-30 10:52:54 +0800

    添加备注，支持了弹窗

--- uniapp-cal-price/src/components/WechatFab.vue
+++ uniapp-cal-price/src/components/WechatFab.vue
@@ -1,15 +1,18 @@
 <template>
   <view class="wx-fab" @click="openCorpWx">
-    <image src="@/static/qywx_icon.png" mode="widthFix" class="wx-icon" />
+    <view class="fab-inner">
+      <image src="@/static/qywx_icon.png" mode="widthFix" class="wx-icon" />
+      <text class="fab-label">领新客优惠</text>
+    </view>
     <uni-popup ref="qrPopup" type="center">
       <view class="qr-wrap">
-        <!-- <image src="@/static/corp_wx_qr.png" mode="widthFix" class="qr-img" /> -->
-        <image src="@/static/qywx_qr_code.png" wode="widthFix" class="qr-img" />
+        <image src="@/static/qywx_qr_code.png" mode="widthFix" class="qr-img" />
         <text class="qr-tip">长按识别，添加企业微信客服</text>
       </view>
     </uni-popup>
   </view>
 </template>
+
 
 <script>
 import uniPopup from '@dcloudio/uni-ui/lib/uni-popup/uni-popup.vue'
@@ -51,11 +54,11 @@ export default {
   position: fixed;
   right: 24rpx;
   bottom: 80rpx;
-  width: 100rpx;
-  height: 100rpx;
+  width: 138rpx;
+  height: 138rpx;
   border-radius: 50%;
-  background: #07C160;
-  box-shadow: 0 8rpx 24rpx rgba(0, 0, 0, 0.18);
+  /* background: #91bcf0; */
+  box-shadow: 0 8rpx 24rpx rgba(0, 0, 0, 0.68);
   display: flex;
   align-items: center;
   justify-content: center;
@@ -80,4 +83,17 @@ export default {
   color: #555;
   margin-top: 24rpx;
 }
+
+.fab-inner {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.fab-label {
+  font-size: 20rpx;
+  color: #000;
+  margin-top: 16rpx;
+}
+
 </style>
